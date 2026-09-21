40adacc0f741dc4567009f48980b57357c693532
Manas Kanakala <147285261+mkanakala10@users.noreply.github.com>
2026-09-20 23:36:41 -0700

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        def clearIsland(grid, x, y):
+            queue = deque([(x, y)])
+            area = 0
+            while queue:
+                i, j = queue.popleft()
+                area += 1
+
+                grid[i][j] = 0
+
+                if i - 1 >= 0 and grid[i - 1][j] == 1 and (i - 1, j) not in visited:
+                    queue.append((i - 1, j))
+                    visited.add((i - 1, j))
+                if i + 1 < len(grid) and grid[i + 1][j] == 1 and (i + 1, j) not in visited:
+                    queue.append((i + 1, j))
+                    visited.add((i + 1, j))
+                if j - 1 >= 0 and grid[i][j - 1] == 1 and (i, j - 1) not in visited:
+                    queue.append((i, j - 1))
+                    visited.add((i, j - 1))
+                if j + 1 < len(grid[0]) and grid[i][j + 1] == 1 and (i, j + 1) not in visited:
+                    queue.append((i, j + 1))
+                    visited.add((i, j + 1))
+            print(area)
+            return area
+        visited = set()
+        res = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    area = clearIsland(grid, i, j)
+                    res = max(res, area)
+        return res
